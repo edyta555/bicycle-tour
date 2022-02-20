7487de7f4eb81e90019394e2a212fe57d527a0f4
edyta555 <edyta.badowska555@gmail.com>
2022-02-20 01:14:57 +0100

    Add SingleTour component.

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import { SingleTour } from "./single-tour/SingleTour";
+
+export const App = () => {
+  return (
+    <div>
+      <SingleTour />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/single-tour/SingleTour.js
@@ -0,0 +1,16 @@
+export const SingleTour = () => {
+  return (
+    <div className="single-tour">
+      <div className="single-tour-data">May 12th 2021</div>
+      <div className="single-tour-description">
+        <div className="single-tour-route">
+          <h2>Warsaw</h2>
+          <h2>Marki</h2>
+        </div>
+        <div className="single-tour-distance">16km</div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleTour;
