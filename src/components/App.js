10ff1ef8cd1f9d2b4a81ccdb5f181caebcaad617
edyta555 <edyta.badowska555@gmail.com>
2022-03-05 14:46:01 +0100

    Render list of tours data dynamically.

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,14 @@ import { toursData } from "./payload";
 const App = () => {
   return (
     <div>
-      <SingleTour
-        date={toursData[0].tourDate}
-        startPoint={toursData[0].startPoint}
-        endPoint={toursData[0].endPoint}
-        distance={toursData[0].tourDistance}
-      />
-      <SingleTour
-        date={toursData[1].tourDate}
-        startPoint={toursData[1].startPoint}
-        endPoint={toursData[1].endPoint}
-        distance={toursData[1].tourDistance}
-      />
-      <SingleTour
-        date={toursData[2].tourDate}
-        startPoint={toursData[2].startPoint}
-        endPoint={toursData[2].endPoint}
-        distance={toursData[2].tourDistance}
-      />
+      {toursData.map((tourData) => (
+        <SingleTour
+          date={tourData.tourDate}
+          startPoint={tourData.startPoint}
+          endPoint={tourData.endPoint}
+          distance={tourData.tourDistance}
+        />
+      ))}
     </div>
   );
 };
--- src/components/payload.js
+++ src/components/payload.js
@@ -1,17 +1,20 @@
 export const toursData = [
   {
+    id: 1,
     tourDate: new Date(2021, 5, 12),
     startPoint: "Warsaw",
     endPoint: "Marki",
     tourDistance: 32,
   },
   {
+    id: 2,
     tourDate: new Date(2021, 9, 10),
     startPoint: "Warsaw",
     endPoint: "Nowy Dwór Mazowiecki",
     tourDistance: 86,
   },
   {
+    id: 3,
     tourDate: new Date(2021, 10, 15),
     startPoint: "Warsaw",
     endPoint: "Legionowo",
