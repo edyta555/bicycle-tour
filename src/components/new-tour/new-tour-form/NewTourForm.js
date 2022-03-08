bc41c3d036b72f4bab94f87ac6f41a2e68ca8a3c
edyta555 <edyta.badowska555@gmail.com>
2022-03-08 23:46:02 +0100

    Add handling multiple inputs.

--- src/components/new-tour/new-tour-form/NewTourForm.js
+++ src/components/new-tour/new-tour-form/NewTourForm.js
@@ -1,31 +1,71 @@
+import { useState } from "react";
 import "./new-tour-form.css";
 
 const NewTourForm = () => {
+  const [enteredData, setEnteredData] = useState({
+    startPoint: "",
+    stopPoint: "",
+    distance: "",
+    date: "",
+  });
+
+  const enteredDataChangeHandler = (event) => {
+    const newValue = event.target.value;
+    setEnteredData((prevState) => {
+      return { ...prevState, [event.target.name]: newValue };
+    });
+  };
+
   return (
     <form>
       <div className="form-fields">
         <div className="form-field">
           <label>
             Start point:
-            <input type="text" />
+            <input
+              type="text"
+              name="startPoint"
+              value={enteredData.startPoint}
+              onChange={enteredDataChangeHandler}
+            />
           </label>
         </div>
         <div className="form-field">
           <label>
             End point:
-            <input type="text" minLength="1" />
+            <input
+              type="text"
+              minLength="1"
+              name="stopPoint"
+              value={enteredData.stopPoint}
+              onChange={enteredDataChangeHandler}
+            />
           </label>
         </div>
         <div className="form-field">
           <label>
             Distance:
-            <input type="number" min="1" step="1" />
+            <input
+              type="number"
+              min="1"
+              step="1"
+              name="distance"
+              value={enteredData.distance}
+              onChange={enteredDataChangeHandler}
+            />
           </label>
         </div>
         <div className="form-field">
           <label>
             Date:
-            <input type="date" min="2018-01-01" max="2022-12-21" />
+            <input
+              type="date"
+              min="2018-01-01"
+              max="2022-12-21"
+              name="date"
+              value={enteredData.date}
+              onChange={enteredDataChangeHandler}
+            />
           </label>
         </div>
       </div>
